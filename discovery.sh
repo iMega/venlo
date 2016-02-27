6d8ebb8779dfaf131da4f6f7848b5a8c28d35196
Dmitry <irvis@imega.ru>
2016-02-27 15:33:57 +0300

    add discover

---
+++ discovery.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#
+# $1 name container
+#
+checkConnectToData() {
+    DATA_IP=$(docker inspect --format '{{ index (index .Config.Env) 0 }}' $1)
+    IFS='=' read -ra IP <<< "$DATA_IP"
+    if [ $TELEPORT_DATA_IP != ${IP[1]} ]; then
+        docker stop $1
+        docker rm -fv $1
+        make $1
+    fi
+}
+
+TELEPORT_DATA_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' teleport_data)
+
+checkConnectToData teleport_inviter
